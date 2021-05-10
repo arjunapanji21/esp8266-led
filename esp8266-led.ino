d4ad09e09c0c1943fa4cce5134119bc108566bfb
arjunapanji21 <arjuna.panji97@gmail.com>
2021-05-10 16:41:13 +0700

    initial commit

--- esp8266-led.ino
+++ esp8266-led.ino
@@ -1,149 +1,96 @@
-/**********************************************
-* 
-* Program : Web Server Kontrol 1 LED
-* https://github.com/arjunapanji21/esp8266-led
-* 
-* *********************************************/
+#include <ESP8266WiFi.h>
 
-// import library WiFi Esp
-#include <WiFiEspServer.h>
-#include "WiFiEsp.h"
-#include <WiFiEspClient.h>
-#include <WiFiEspUdp.h>
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
 
-// import library komunikasi serial
-#include <SoftwareSerial.h>
+const int LED1 = 5;
 
-// import library lcd
-#include <LiquidCrystal_I2C.h>
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
 
-SoftwareSerial Serial1(8, 9); // RX, TX pada ESP8266
-LiquidCrystal_I2C lcd(0x27, 16, 2); // lcd yang digunakan yaitu 16x2
+void setup() {
+  Serial.begin(115200);
 
-char ssid[] = "Ursi Aulia"; // nama wifi
-char pass[] = "aulia123"; // password wifi
+  // prepare LED
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // digitalWrite(LED_BUILTIN, 0);
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, LOW);
 
-int status = WL_IDLE_STATUS; // status server
-
-WiFiEspServer server(80); // mengaktifkan port 80
-WiFiEspClient client; // inisialisasi ethernet client object
-RingBuffer buf(8);
-
-#define LED1 10 // pin LED pada ESP8266
-int ledStatus = LOW; // status awal led yaitu LOW alias Off
-
-void setup(){
-  lcd.init(); // inisialisasi LCD
-  lcd.backlight(); // mengaktifkan lampu lcd
-  lcd.setCursor(0,0); // memposisikan cursor lcd
-  lcd.print("SSID: "); 
-  lcd.print(ssid); // menampilkan nama wifi kedalam lcd
-
-  pinMode(LED1, OUTPUT); // menetapkan pin pada variable LED1 sebagai OUTPUT
-  digitalWrite(LED1, LOW); // memastikan lampu led pada kondisi off ketika program baru jalankan
-
-  Serial.begin(9600); // mengaktifkan Serial Monitor
-  Serial1.begin(9600); // mengaktifkan komunikasi serial
-
-  WiFi.init(&Serial1); // inisialisasi modul WiFi
-
-  if(WiFi.status() == WL_NO_SHIELD){
-    Serial.println("WiFi Shild tidak tersedia"); // jika WiFi Shield tidak ada
-    lcd.setCursor(0,1);
-    lcd.print("Tidak terhubung");
-    while(true); //?
-  }
-
-  while (status != WL_CONNECTED) {
-    Serial.print("Menghubungkan ke SSID: "); // jika belum terhubung
-    Serial.println(ssid);
-    lcd.setCursor(0,1);
-    lcd.print("Menghubungkan...");
-    status = WiFi.begin(ssid, pass);
-  }
-
-  if(status == WL_CONNECTED){
-    Serial.println("Berhasil terhubung ke jaringan"); // jika sudah terhubung    
-  }
-
-  printWifiStatus();
-
-  //lcd.clear();
-
-  server.begin(); // mulai menjalankan server
-}
-
-void loop(){
-  client = server.available();
-  if(client){
-    Serial.println("Client Baru Terhubung");
-    buf.init();
-    while(client.connected()){
-      if(client.available()){
-        char c = client.read();
-        buf.push(c);
-        if(buf.endsWith("\r\n")){
-          sendHttpResponse(client);
-          break;
-        }
-        lcd.setCursor(0,1);
-        if (buf.endsWith("GET /H1")) {
-          digitalWrite(LED1, HIGH); 
-          lcd.print("LED 1 ON ");
-        }
-        if (buf.endsWith("GET /L1")) {
-          digitalWrite(LED1, LOW);
-          lcd.print("LED 1 OFF");
-        }
-      }
-    }
-    client.stop();
-    Serial.println("Client disconnected");
-  }
-}
-
-void sendHttpResponse(WiFiEspClient client)
-{
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-type:text/html");
-  client.println();
-  client.println("");
-  client.println("<hr/><hr>");
-  client.println("<h4><center> IoT Starter Kit Arduino : Kontrol</center></h4>");
-  client.println("<hr/><hr>");
-  client.println("<br><br>");
-  client.println("<center>");
-  client.println("LED 1");
-  client.println("<a href=\"/H1\"><button>Turn On </button></a>");
-  client.println("<a href=\"/L1\"><button>Turn Off </button></a><br />");
-  client.println("</center>");
-  client.println("<br><br>");
-  client.println("<center>");
-  if (digitalRead(LED1)) {
-    client.print("<td>LED 1 = ON</td>");
-  }
-  else {
-    client.print("<td>LED 1 = OFF</td>");
-  }
-  client.println();
-}
-void printWifiStatus()
-{
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  // Connect to WiFi network
   Serial.println();
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
   Serial.println();
-  lcd.setCursor(0, 1);
-  lcd.print("Terhubung");
-  delay(2000);
-  lcd.clear();
-  lcd.print("Alamat IP");
-  lcd.setCursor(0, 1);
-  lcd.print(ip);
-  delay(2000);
+  Serial.print(F("Connecting to "));
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+  Serial.println(F("WiFi connected"));
+
+  // Start the server
+  server.begin();
+  Serial.println(F("Server started"));
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {  
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println(F("new client"));
+
+  client.setTimeout(5000); // default is 1000
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(F("request: "));
+  Serial.println(req);
+
+  // Match the request
+  int val;
+  if (req.indexOf(F("/gpio/0")) != -1) {
+    val = 0;
+  } else if (req.indexOf(F("/gpio/1")) != -1) {
+    val = 1;
+  } else {
+    Serial.println(F("invalid request"));
+    val = digitalRead(LED1);
+  }
+
+  // Set LED according to the request
+  digitalWrite(LED1, val);
+
+  // read/ignore the rest of the request
+  // do not client.flush(): it is for output only, see below
+  while (client.available()) {
+    // byte by byte is not very efficient
+    client.read();
+  }
+
+  // Send the response to the client
+  // it is OK for multiple small client.print/write,
+  // because nagle algorithm will group them into one single packet
+  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now "));
+  client.print((val) ? F("high") : F("low"));
+  client.print(F("<br><br>Click <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/1'>here</a> to switch LED GPIO on, or <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/0'>here</a> to switch LED GPIO off.</html>"));
+
+  // The client will actually be *flushed* then disconnected
+  // when the function returns and 'client' object is destroyed (out-of-scope)
+  // flush = ensure written data are received by the other side
+  Serial.println(F("Disconnecting from client"));
 }
